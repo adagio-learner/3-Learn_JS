f896d7c5c2d0d6e433e598db9c354937c36b5a9e
adagio_learner <social.ayan.khan@gmail.com>
2025-06-08 17:23:06 +0530

    add break

--- 3.loops_and_string/index.js
+++ 3.loops_and_string/index.js
@@ -1,8 +1,12 @@
 console.log("Forward Counting")
 for(let i=0; i<5; i++){
-    console.log(i+1)
+    console.log(i+1);
 }
 console.log("Reverse Counting")
 for(let i=5; i>0; i--){
-    console.log(i)
+    if (i==2) {
+        break;
+    }else{
+        console.log(i);
+    }
 }
