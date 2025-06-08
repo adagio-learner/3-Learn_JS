76b06fb0a5f67d48ada1582173fa16503f51a8ab
adagio_learner <social.ayan.khan@gmail.com>
2025-06-08 17:33:06 +0530

    add continue

--- 3.loops_and_string/index.js
+++ 3.loops_and_string/index.js
@@ -2,6 +2,7 @@ console.log("Forward Counting")
 for(let i=0; i<5; i++){
     console.log(i+1);
 }
+
 console.log("Reverse Counting")
 for(let i=5; i>0; i--){
     if (i==2) {
@@ -9,4 +10,13 @@ for(let i=5; i>0; i--){
     }else{
         console.log(i);
     }
+}
+
+console.log()
+for(let i=0; i<5; i++){
+    if(i==3){
+        continue;
+    }else(
+        console.log(i+1)
+    )
 }
