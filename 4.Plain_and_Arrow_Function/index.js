57ea0a9ca36a141a29f004b865c2e4ac351bb4bb
adagio_learner <social.ayan.khan@gmail.com>
2025-06-10 16:32:11 +0530

    create print funtion and counting funtion

---
+++ 4.Plain_and_Arrow_Function/index.js
@@ -0,0 +1,17 @@
+// function defination
+function sayMyName(){
+    console.log("Heisenberg from Breaking Bad")
+}
+
+function printCounting(){
+    for(let i=1; i<=10; i++){
+        console.log(i)
+    }
+}
+
+
+// function use - function call
+sayMyName();
+
+printCounting()
+
