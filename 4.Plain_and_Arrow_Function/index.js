a0cf6b8afd42ced385d650f302f8a5e6da2f5af3
adagio_learner <social.ayan.khan@gmail.com>
2025-06-10 16:56:49 +0530

    Create a return function and call them

--- 4.Plain_and_Arrow_Function/index.js
+++ 4.Plain_and_Arrow_Function/index.js
@@ -14,6 +14,18 @@ function printSquare(num){
     console.log("Printing Square:",num);
 }
 
+// Return Function
+function getSum(a, b, c){
+    let sum = a+b+c;
+    return sum;
+}
+
+function getMyName(firstName, lastName) {
+    let fullName = firstName+" "+lastName;
+    return fullName;
+}
+
+
 
 // function use - function call
 sayMyName();
@@ -22,4 +34,13 @@ printCounting()
 
 // 5 is the argument
 printSquare(5)
+
+
+// Function call a Return function
+let ans = getSum(34, 55, 54);
+console.log("Printing Sum:",ans);
+
+let yourName = getMyName("Walter", "White");
+console.log("Full Name:",yourName);
+
 
