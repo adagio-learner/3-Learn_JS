4fec60ee8562c054eb718e92a5da3544167067c6
adagio_learner <social.ayan.khan@gmail.com>
2025-06-12 23:33:43 +0530

    add object

---
+++ 5.Object_and_Array/index.js
@@ -0,0 +1,17 @@
+// Reference type Datatype
+// 1) Array => collection of item
+// 2) Object => collection of key value pair
+// 3) Function
+
+let obj = {
+    "full name": "adagio lerner",
+    age: 25,
+    weight: 85,
+    height: "5ft 8in",
+    greet: function(){
+        console.log("Hello everybody how are you");
+    }
+};
+
+console.log(obj);
+obj.greet();
