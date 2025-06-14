1137ca5cd24f51c29315a6db6e328d288df7a131
adagio_learner <social.ayan.khan@gmail.com>
2025-06-14 16:30:17 +0530

    learn global scoped

---
+++ 7.VariableScoping/index.js
@@ -0,0 +1,24 @@
+// Variable scoping
+
+// Global Scoped
+// let age = 15;
+// const age = 15;
+var age = 15;
+
+console.log(age);
+
+{
+    console.log(age-1);
+}
+
+function tellAge(){
+    console.log(age-3)
+}
+
+if (true){
+    console.log(age-2)
+}
+
+for(let i=0; i<2; i++){
+    console.log(age+i);
+}
