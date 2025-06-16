fca4e3c45aab2ae909f7a74a1cfe691a981e6ba4
adagio_learner <social.ayan.khan@gmail.com>
2025-06-16 23:13:09 +0530

    push: 9.inBuiltObject

--- 9.inBuiltObject/index.js
+++ 9.inBuiltObject/index.js
@@ -31,3 +31,15 @@ console.log(Math.sqrt(25))
 
 // print the multiplication of first number till second number times
 console.log(Math.pow(2, 10))
+
+
+let curr = new Date()
+let date = new Date("June 29 1988 05:21")
+let newDate = new Date(1988, 5, 28, 4)
+
+console.log(curr)
+console.log(date)
+console.log(newDate)
+console.log(newDate.getDay())
+console.log(newDate.setFullYear(2001));
+console.log(newDate)
