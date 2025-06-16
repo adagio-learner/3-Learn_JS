2e69ecd4bd8a0af7bf5e902e961c52288c544003
adagio_learner <social.ayan.khan@gmail.com>
2025-06-16 23:11:47 +0530

    add math in build object: round,floor,ceil

--- 9.inBuiltObject/index.js
+++ 9.inBuiltObject/index.js
@@ -6,3 +6,15 @@ console.log(Math.max(60, 30, 24, 200, 712));
 
 // print minimum
 console.log(Math.min(60, 30, 24, 200, 712));
+
+// print round off value
+console.log(Math.round(1.4));
+console.log(Math.round(1.5));
+
+// print previous closest round off
+console.log(Math.floor(1.4));
+console.log(Math.floor(1.9));
+
+// print next closest round off
+console.log(Math.ceil(1.1));
+console.log(Math.ceil(1.9));
