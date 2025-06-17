5e2629192ad9f0dbf60bb7a42bed3a099a1c2253
adagio_learner <social.ayan.khan@gmail.com>
2025-06-17 23:19:37 +0530

    Object cloning using assign

--- 10.objectCloningAndGarbageCollector/index.js
+++ 10.objectCloningAndGarbageCollector/index.js
@@ -3,6 +3,12 @@ let src = {
     wt: 68,
     ht: 180
 }
+
+let src1 = {
+    value:101,
+    name:"Adagio Learner"
+}
+
 console.log(src)
 
 src.color = "white";
@@ -14,7 +20,11 @@ console.log("thing:",thing);
 
 // Object cloning using Spread oprator
 let dest = {...src}
+console.log("Clone by spread oprator:",dest)
+
+// Object cloning using assign
+let assignDest = Object.assign({}, src, src1)
+console.log("clone by assign:",assignDest)
+
 src.age = 15;
 console.log("src:",src)
-console.log("dest:",dest)
-
