0753850d41f79f07cffb7190a6a45583b1661e98
adagio_learner <social.ayan.khan@gmail.com>
2025-06-17 23:31:57 +0530

    Object cloning using iteration

--- 10.objectCloningAndGarbageCollector/index.js
+++ 10.objectCloningAndGarbageCollector/index.js
@@ -9,22 +9,33 @@ let src1 = {
     name:"Adagio Learner"
 }
 
-console.log(src)
+console.log(src);
 
 src.color = "white";
-console.log("src:",src)
+console.log("src:",src);
 
 let thing = src;
 // thing and src are the reference of same srcect
 console.log("thing:",thing);
 
 // Object cloning using Spread oprator
-let dest = {...src}
-console.log("Clone by spread oprator:",dest)
+let dest = {...src};
+console.log("Clone by spread oprator:",dest);
 
 // Object cloning using assign
-let assignDest = Object.assign({}, src, src1)
-console.log("clone by assign:",assignDest)
+let assignDest = Object.assign({}, src, src1);
+console.log("Clone by assign:",assignDest);
 
 src.age = 15;
-console.log("src:",src)
+console.log("src:",src);
+
+// Object cloning using iteration
+let iterationDest = {};
+
+for(let key in src){
+    let newKey = key;
+    let newValue = src[key];
+    // inset newKey and value in dest and create a clone
+    iterationDest[newKey] = newValue
+}
+console.log("Clone by iteration",iterationDest)
