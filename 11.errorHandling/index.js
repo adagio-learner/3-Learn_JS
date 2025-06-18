66e57be4c71b6d93faa3c1fff7a854fe64c6074e
adagio_learner <social.ayan.khan@gmail.com>
2025-06-18 23:31:39 +0530

    add try-catch block

--- 11.errorHandling/index.js
+++ 11.errorHandling/index.js
@@ -1,7 +1,18 @@
-// Compile time error
-// syntax error
-console.log(1;
+// // Compile time error
+// // syntax error
+// console.log(1;
 
-// Runtime error
-// runtime error
-console.log(x);
+// // Runtime error
+// // runtime error
+// console.log(x);
+
+try{
+    console.log("try block starts here");
+    console.log(age);
+    console.log("try block ends here");
+}
+catch(err){
+    // define how to handle error or what to do when error appear
+    console.log("I am inside catch block")
+    console.log("Your error is here:",err)
+}
