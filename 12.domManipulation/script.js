72fbeda577660339d7639b7e611143c20f4c8e50
adagio_learner <social.ayan.khan@gmail.com>
2025-06-19 18:21:19 +0530

    remove

--- 12.domManipulation/script.js
+++ 12.domManipulation/script.js
@@ -1,3 +1,4 @@
+// create  and add element
 let myDiv = document.querySelector("#myDiv");
 
 let newElement = document.createElement("span")
@@ -5,3 +6,9 @@ newElement.textContent = "adagio learner"
 
 
 myDiv.insertAdjacentElement("beforebegin", newElement);
+
+
+let parent = document.querySelector("#myDiv");
+// let parent = child.parent
+let child = document.querySelector("#firstPara");
+parent.removeChild(child)
