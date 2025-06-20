60353d44beb1edb9a06142380fcabb057773e113
adagio_learner <social.ayan.khan@gmail.com>
2025-06-20 23:38:03 +0530

    add .cssText

---
+++ 13.domManipulationStyleElement/script.js
@@ -0,0 +1,13 @@
+let paraElement = document.getElementById("sPara");
+
+paraElement.style.backgroundColor = "#ffafcc";
+
+
+
+let secondElement = document.getElementById("sDiv");
+// secondElement.style.cssText;
+secondElement.style.cssText ="background-color: #669bbc; color: white; padding: 0.5rem";
+
+
+
+
