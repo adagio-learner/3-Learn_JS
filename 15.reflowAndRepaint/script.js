471088924cd668bb333e8ed9124c002caa53a551
adagio_learner <social.ayan.khan@gmail.com>
2025-06-22 22:31:21 +0530

    create a function for creating 100 para

---
+++ 15.reflowAndRepaint/script.js
@@ -0,0 +1,19 @@
+
+
+// code   1
+
+for(let i=1; i<=100; i++){
+    let para = document.createElement("p");
+    para.textContent = "This is para " + i;
+    document.body.append(para)
+}
+
+
+
+
+
+// code   2
+
+
+
+
