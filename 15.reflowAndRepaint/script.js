fd5a109bc2fcf926ffc1114859548cebdd218032
adagio_learner <social.ayan.khan@gmail.com>
2025-06-22 22:35:58 +0530

    claculate a time taken by code 1

--- 15.reflowAndRepaint/script.js
+++ 15.reflowAndRepaint/script.js
@@ -1,14 +1,17 @@
 
 
 // code   1
+const t1 = performance.now()
 
 for(let i=1; i<=100; i++){
     let para = document.createElement("p");
     para.textContent = "This is para " + i;
-    document.body.append(para)
+    document.body.append(para);
 }
 
+const t2 = performance.now();
 
+console.log("total time taken by code 1: " + (t2-t1));
 
 
 
