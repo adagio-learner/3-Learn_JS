0ed144675e3619222a1251eccf9954e8a2aa5d2b
adagio_learner <social.ayan.khan@gmail.com>
2025-06-24 23:38:52 +0530

    add: promise for asyn code

--- 17.Promises/script.js
+++ 17.Promises/script.js
@@ -3,7 +3,19 @@ let firstPromise = new Promise( (resolve, reject) =>{
     resolve(1001);
 } );
 
-let secondPromise = new Promise( (resolve, reject) =>{
-    console.log("adagio learner");
-    reject(new Error("Internal Server Error"));
-} );
+// let secondPromise = new Promise( (resolve, reject) =>{
+    //     console.log("adagio learner");
+    //     reject(new Error("Internal Server Error"));
+    // } );
+    
+    
+    let thirdPromise = new Promise( (resolve, reject) =>{
+
+        function sayMyName() {
+            console.log("My Name is Adagio Learner");
+        }
+        setTimeout(sayMyName, 10000);
+        resolve(1);
+
+    } );
+
