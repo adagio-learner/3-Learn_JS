1470578b279dc4d9f95b111cf918402ca557a95d
adagio_learner <social.ayan.khan@gmail.com>
2025-06-24 23:31:38 +0530

    add: promise with reject

--- 17.Promises/script.js
+++ 17.Promises/script.js
@@ -2,3 +2,8 @@ let firstPromise = new Promise( (resolve, reject) =>{
     console.log("adagio learner");
     resolve(1001);
 } );
+
+let secondPromise = new Promise( (resolve, reject) =>{
+    console.log("adagio learner");
+    reject(new Error("Internal Server Error"));
+} );
