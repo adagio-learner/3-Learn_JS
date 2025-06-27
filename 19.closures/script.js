68b07db969fcb077f917e22f9204866cdeeed4f4
adagio_learner <social.ayan.khan@gmail.com>
2025-06-27 23:45:06 +0530

    add nested function displayName

---
+++ 19.closures/script.js
@@ -0,0 +1,12 @@
+// closure = { function + required data(or)lexical scope(or)surrounding state }
+
+// nested function
+function init(){
+    var name = "Adagio"; // name is a local variable created by init
+    function displayName(){
+        // displayName() is the inner function, that forms a closure
+        console.log(name); // use variable declared in the parent function
+    }
+    displayName();
+}
+init();
